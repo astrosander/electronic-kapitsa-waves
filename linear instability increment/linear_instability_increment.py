932b292b1a16a368a9c1c6e2d5805281b24895ca
Aliaksandr Melnichenka <togpe22@gmail.com>
2025-08-30 12:39:56 -0400

    Tuned linear instability simulation to fit non-injection input parameters

--- linear instability increment/linear_instability_increment.py
+++ linear instability increment/linear_instability_increment.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-U0 = 0.5
-eta_p = 0.01
-eta_n = 0.01
-n = 1
-w = 0.5
-gamma0 = 10
-N = 200
-kmin = -8
-kmax = 8
+U0 = 1.0
+eta_p = 5e-3
+eta_n = 5e-3
+n = 0.2
+w = 5.0
+gamma0 = 2.50
+N = 512
+kmin = -20
+kmax = 20
 Lambda = -(1 / w + 1 / n) * np.exp(-n / w)
 fig, ax = plt.subplots()
 
-u_values = [0.6, 0.5, 0.4, 0.3]
+u_values = [20.0]
 
 for u in u_values:
     k_out = np.linspace(kmin, kmax, N)
@@ -38,12 +38,30 @@ for u in u_values:
     ax.plot(k_out, np.imag(omega1), label=f'u = {u}', linewidth=1.0)
     ax.plot(k_out, np.imag(omega2), linewidth=1.0)
 
+
+mask = k_out >= 0
+k_right = k_out[mask]
+im1_right = np.imag(omega1)[mask]
+k_max_right = k_right[np.argmax(im1_right)]
+ax.axvline(k_max_right, color="blue", linestyle="--", linewidth=1.2,
+           label=f"max at k={k_max_right:.2f}")
+
+L = 10.0
+k_line = 6 * np.pi / L
+ax.axvline(k_line, color="red", linestyle="--", linewidth=1.2, label=f"$k = 6\\pi/{L:.0f}$")
+
+# k_line = 20 * np.pi / L
+# ax.axvline(k_line, color="purple", linestyle="--", linewidth=1.2, label=f"$k = 6\\pi/{L:.0f}$")
+
+# k_line = 12 * np.pi / L
+# ax.axvline(k_line, color="green", linestyle="--", linewidth=1.2, label=f"$k = 6\\pi/{L:.0f}$")
+
 ax.grid(True)
 ax.set_xlabel('Wavenumber k')
 ax.set_ylabel('Instability increment')
-ax.set_ylim([-3, 1])
-ax.set_xlim([-10, 10])
+# ax.set_ylim([-5, 5])
+# ax.set_xlim([-20, 20])
 ax.legend(title='Drift velocity (u)')
 ax.set_title('Instability increments vs Wavenumber')
 
-plt.tight_layout(); plt.savefig(f"linear_instability_increment.png", dpi=160); plt.close()
+plt.tight_layout(); plt.savefig(f"linear_instability_increment.png", dpi=160); plt.show()#plt.close()
