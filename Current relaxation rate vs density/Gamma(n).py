7e5280bb6f1fb570e92aa078d4d8506f575ca0ef
Aliaksandr Melnichenka <69795340+astrosander@users.noreply.github.com>
2025-12-10 19:08:25 -0500

    remove the ref lines and colorbar

--- Current relaxation rate vs density/Gamma(n).py
+++ Current relaxation rate vs density/Gamma(n).py
@@ -103,7 +103,6 @@ temperatures = np.array([20, 30, 40, 50, 70, 90, 110, 140, 170, 200, 250, 300])
 temperatures = temperatures/10
 temperatures = np.array([2, 4, 8, 16, 32])
 INCLUDE_INTERBAND = False
-USE_COLORBAR = True
 
 cmap = plt.get_cmap("plasma")
 norm = mpl.colors.Normalize(vmin=temperatures.min(), vmax=temperatures.max())
@@ -156,27 +155,6 @@ for T in temperatures:
 ax.set_xlim(n_min, n_max)
 ax.set_ylim(y_min, y_max)
 
-n_ref = np.sqrt(n_min * n_max)
-y_ref = np.sqrt(y_min * y_max)
-
-n_slope1 = np.logspace(np.log10(n_min), np.log10(n_max), 100)
-y_slope1 = y_ref * (n_slope1 / n_ref)**2*20
-# ax.loglog(n_slope1, y_slope1, 'k:', alpha=0.4, linewidth=1.0, zorder=0)
-n_text1 = n_max * 0.1
-y_text1 = y_ref * (n_text1 / n_ref)**2*30
-# ax.text(n_text1, y_text1, r'$n^2$', rotation=45, ha='left', va='bottom', 
-#         fontsize=7, alpha=0.9, color='#2C3E50')
-
-n_slope_neg1_full = np.logspace(np.log10(n_min), np.log10(n_max), 100)
-mask_neg1 = n_slope_neg1_full > 1e12
-n_slope_neg1 = n_slope_neg1_full[mask_neg1]
-y_slope_neg1 = y_ref * (n_slope_neg1 / n_ref)**(-1)*3
-ax.loglog(n_slope_neg1, y_slope_neg1,  alpha=0.4, linewidth=1.0, zorder=0)
-n_text_neg1 = max(5e11 * 1.1, n_max * 0.5)
-y_text_neg1 = y_ref * (n_text_neg1 / n_ref)**(-1)*3
-ax.text(n_text_neg1, y_text_neg1, r'$n^{-1}$', rotation=-45, ha='left', va='top', 
-        fontsize=7, alpha=0.9, color='#2C3E50')
-
 ax.set_xlabel(r"density $n$ (cm$^{-2}$)", color="#2C3E50")
 ax.set_ylabel(r"current relaxation rate $\Gamma_J$ (s$^{-1}$)", color="#2C3E50")
 
@@ -188,19 +166,6 @@ ax.xaxis.set_minor_formatter(LogFormatter(minor_thresholds=(2, 0.4)))
 ax.yaxis.set_minor_formatter(LogFormatter(minor_thresholds=(2, 0.4)))
 
 ax.tick_params(colors="#2C3E50")
-
-if USE_COLORBAR:
-    sm = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
-    sm.set_array([])
-    cbar = fig.colorbar(sm, ax=ax, pad=0.02, fraction=0.05)
-    cbar.set_label("$T$ (K)", color="#2C3E50")
-    cbar.ax.tick_params(colors="#2C3E50")
-    cbar.outline.set_edgecolor("#2C3E50")
-    cbar.outline.set_linewidth(0.8)
-else:
-    for T in temperatures[::2]:
-        ax.plot([], [], color=cmap(norm(T)), label=f"{T} K")
-    ax.legend(frameon=False, loc="best", ncols=2)
 
 fig.tight_layout(pad=0.4)
 
